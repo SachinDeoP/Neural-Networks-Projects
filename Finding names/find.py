0227355c1ff25d0856003b5e56d596345050666d
Sachin.deo <sachindeo.prasad@genpact.digital>
2022-08-22 20:05:28 +0530

    Adding finding names

---
+++ Finding names/find.py
@@ -0,0 +1,25 @@
+import os
+import re
+import pandas as pd
+
+os.chdir("C:\\Users\\703294213\\Documents\\Work\\Names")
+# data = pd.read_csv("Indian-Female-Names.csv")
+data = pd.read_excel('India_Baby_Names.xls', sheet_name=0)
+
+name_list_1 = data['Name_1'].to_list()
+name_list_2 = data['Name_2'].to_list()
+name_list = name_list_1 + name_list_2
+
+name_list = [x for x in name_list if not isinstance(x, int)]
+name_list = [x for x in name_list if not isinstance(x, float)]
+only_alpha = []
+for string in range(len(name_list)):
+    if name_list[string].isalpha():
+        only_alpha.append(name_list[string])
+    else:
+        pass
+r = re.compile(".*[s].*[p].*")
+selected_list = list(filter(r.match, only_alpha))
+
+
+print("Done")
